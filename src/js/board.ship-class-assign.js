3a06eaf0cf7155a040a7e5da9e4ca0608e191601
ihascats <s.pavlovic.rs@gmail.com>
2022-06-11 20:45:46 +0200

    Link images from github

--- src/js/board.ship-class-assign.js
+++ src/js/board.ship-class-assign.js
@@ -18,9 +18,10 @@ function assignClass(shipIdNumber, player, playerBoard, row, column) {
   for (let iteration = 0; iteration < 5; iteration += 1) {
     if (shipIdNumber === iteration) {
       const horizontal = document.createElement('img');
-      horizontal.src = `../src/img/${shipType[iteration]}Horizontal.png`;
+      horizontal.src = `https://github.com/ihascats/battleship/blob/main/src/img/${shipType[iteration]}Horizontal.png`;
       const vertical = document.createElement('img');
       vertical.src = `../src/img/${shipType[iteration]}Vertical.png`;
+      vertical.src = `https://github.com/ihascats/battleship/blob/main/src/img/${shipType[iteration]}Vertical.png`;
       const shipId = shipIdNumber;
       const { orientation } = player.map.ships[shipId];
       board.children.item(i * 10 + j).classList.add(shipType[iteration]);
